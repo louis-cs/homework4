14bd09cb53f95c602e11f4e86d4d5a83a5abc94e
lchauvet <lchauvet@postech.ac.kr>
2019-10-18 00:10:39 +0900

    implemented ToStringVisitor methods

--- homework4/src/main/java/edu/postech/csed332/homework4/ToStringVisitor.java
+++ homework4/src/main/java/edu/postech/csed332/homework4/ToStringVisitor.java
@@ -1,11 +1,48 @@
 package edu.postech.csed332.homework4;
 
+import edu.postech.csed332.homework4.expression.*;
+
 /**
  * A visitor to compute the string expression of a given expression
  */
 public class ToStringVisitor implements ExpVisitor<String> {
-
     // TODO write and implement the visitor methods for ToStringVisitor, satisfying
     //  the specification of Exp.toString. A double value is written using exponents,
     //  e.g., using Double.toString().
+
+    @Override
+    public String visitVariableExp(VariableExp v) {
+        return ("x" + v.getName());
+    }
+
+    @Override
+    public String visitNumberExp(NumberExp v) {
+        return v.getValue().toString();
+    }
+
+    @Override
+    public String visitPlusExp(PlusExp v) {
+        return (v.getLeft().toString() + " + " + v.getRight().toString());
+    }
+
+    @Override
+    public String visitMinusExp(MinusExp v) {
+        return (v.getLeft().toString() + " - " + v.getRight().toString());
+    }
+
+    @Override
+    public String visitMultiplyExp(MultiplyExp v) {
+        return (v.getLeft().toString() + " * " + v.getRight().toString());
+    }
+
+    @Override
+    public String visitDivideExp(DivideExp v) {
+        return (v.getLeft().toString() + " / " + v.getRight().toString());
+    }
+
+    @Override
+    public String visitExponentiationExp(ExponentiationExp v) {
+        return (v.getLeft().toString() + " ^ " + v.getRight().toString());
+    }
+    
 }
